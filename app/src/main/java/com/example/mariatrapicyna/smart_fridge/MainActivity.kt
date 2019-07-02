47379a6ed4ef2f356339e5d38d89dba702f12fac
Dsvag <DSVAG@users.noreply.github.com>
2019-07-02 14:36:27 +0300

    add login and register activity

--- app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
+++ app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mariatrapicyna.smart_fridge
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        var email = findViewById<EditText>(R.id.email)
+        var password = findViewById<EditText>(R.id.password)
+        var singIn = findViewById<Button>(R.id.singIn)
+        var register = findViewById<Button>(R.id.register)
+
+        register.setOnClickListener {
+            this.startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+
     }
 }
