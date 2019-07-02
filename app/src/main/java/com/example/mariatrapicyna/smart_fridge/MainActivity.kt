e33251326503e07c2bed4c0b3dfb3fd09f9fd772
Dsvag <DSVAG@users.noreply.github.com>
2019-07-02 14:55:44 +0300

    r

--- app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
+++ app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mariatrapicyna.smart_fridge
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
@@ -11,15 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         var email = findViewById<EditText>(R.id.email)
         var password = findViewById<EditText>(R.id.password)
         var singIn = findViewById<Button>(R.id.singIn)
         var register = findViewById<Button>(R.id.register)
 
-        register.setOnClickListener {
+        register.setOnClickListener(View.OnClickListener {
             this.startActivity(Intent(this, RegisterActivity::class.java))
-        }
-
-
+        })
     }
 }
