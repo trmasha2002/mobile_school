3f111cb44d7ed03b1eceee32670d324e115b38d0
Мария Тряпицына <mariatrapicyna@MacBook-Pro-Maria-3.local>
2019-07-03 02:32:11 +0300

    try_make_registration

--- app/src/main/java/com/example/mariatrapicyna/smart_fridge/register.kt
+++ app/src/main/java/com/example/mariatrapicyna/smart_fridge/register.kt
@@ -1,10 +1,12 @@
 package com.example.mariatrapicyna.smart_fridge
 
 import android.app.ProgressDialog
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -15,6 +17,7 @@ class register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        initialise()
     }
     private var etFirstName: EditText? = null
     private var etLastName: EditText? = null
@@ -25,4 +28,35 @@ class register : AppCompatActivity() {
     private var mDatabaseReference: DatabaseReference? = null
     private var mDatabase: FirebaseDatabase? = null
     private var mAuth: FirebaseAuth? = null
+    private val TAG = "CreateAccountActivity"
+    //global variables
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var email: String? = null
+    private var password: String? = null
+    private fun initialise() {
+        etFirstName = findViewById<View>(R.id.et_first_name) as EditText
+        etLastName = findViewById<View>(R.id.et_last_name) as EditText
+        etEmail = findViewById<View>(R.id.et_email) as EditText
+        etPassword = findViewById<View>(R.id.et_password) as EditText
+        btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
+        mProgressBar = ProgressDialog(this)
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+        btnCreateAccount!!.setOnClickListener { createNewAccount() }
+    }
+    private fun createNewAccount() {
+        firstName = etFirstName?.text.toString()
+        lastName = etLastName?.text.toString()
+        email = etEmail?.text.toString()
+        password = etPassword?.text.toString()
+        if (!TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName)
+            && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgressBar!!.setMessage("Registering User...")
+            mProgressBar!!.show()
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
