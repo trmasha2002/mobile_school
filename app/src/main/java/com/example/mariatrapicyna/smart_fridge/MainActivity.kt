ff9b53b4bfe24b15da1c1323b1d60b7e1e97ae77
Suchenochka <ananas-pod-kaifom@yandex.ru>
2019-07-02 13:51:56 +0300

    add database

--- app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
+++ app/src/main/java/com/example/mariatrapicyna/smart_fridge/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.mariatrapicyna.smart_fridge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.FirebaseDatabase.getInstance
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = getInstance()
+        val myRef = database.reference
+        myRef.child("users").setValue("Hello, World!")
+
     }
+
 }
